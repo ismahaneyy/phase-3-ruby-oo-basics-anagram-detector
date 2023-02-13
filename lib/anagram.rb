88a99d4c493d7d5e9eb7b5f627baa5ffd6f388dc
ismahaneyy <liciousmayrah@gmail.com>
2023-02-13 13:51:56 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram 
+    def initialize (word)
+        @word = word
+    end
+    def match (array_of_words)
+     array_of_words.select do |w|
+        w.split("").sort == @word.split("").sort
+     end
+    end
+end
